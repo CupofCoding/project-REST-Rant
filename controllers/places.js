d0b20368bb7340e854200796617779437a6d6276
Cup of Coding <anne@cupofcoding.com>
2022-04-04 17:28:31 -0700

    updated per assignment

--- controllers/places.js
+++ controllers/places.js
@@ -27,14 +27,25 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+outer.post("/", (req, res) => {
   db.Place.create(req.body)
     .then(() => {
       res.redirect("/places");
     })
     .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
+      if (err && err.name == "ValidationError") {
+        let message = "Validation Error: ";
+
+        //  ToDo: Find all Validation errors
+        for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}.`;
+          message += `${err.errors[field].message}`;
+        }
+        console.log("Validation error message", message);
+        res.render("places/new", { message });
+      } else {
+        res.render("error404");
+      }
     });
 });
 
